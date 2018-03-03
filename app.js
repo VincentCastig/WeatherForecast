1c8e08ee46858a0284f87e25c18e51653924b391
VincentCastig <vincent.castig@gmail.com>
2018-03-03 15:38:49 -0600

    Sharing Name of City Data Between Routes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Moduleddd
+//Module and routes
 var myApp = angular.module('myApp', ['ui.router', 'ngResource'])
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -7,10 +7,28 @@ var myApp = angular.module('myApp', ['ui.router', 'ngResource'])
     $stateProvider
     .state('/', {
         url: '/home',
-        templateUrl: './templates/home.html'
+        templateUrl: './templates/home.html',
+        controller: 'homeController'
     })
     .state('forecast', {
         url: '/forecast',
-        templateUrl: './templates/forecast.html'
+        templateUrl: './templates/forecast.html',
+        controller: 'forecastController'
     })
+});
+
+//services
+myApp.service('cityService', function() {
+    this.city = 'New York, NY'
+});
+//controllers
+myApp.controller('homeController', function($scope, cityService){
+    $scope.city = cityService.city;
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    })
+});
+
+myApp.controller('forecastController', function($scope, cityService){
+    $scope.city = cityService.city;
 });
