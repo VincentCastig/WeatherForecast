c66f29260571ed2a08dccd3c86ba0bbb533c139b
VincentCastig <vincent.castig@gmail.com>
2018-03-03 14:39:14 -0600

    Routing to Weather and Home Page

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 //Moduleddd
-var myApp = angular.module('myApp', ['ui.router', 'ngResource']);
+var myApp = angular.module('myApp', ['ui.router', 'ngResource'])
+.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+    .state('/', {
+        url: '/home',
+        templateUrl: './templates/home.html'
+    })
+    .state('forecast', {
+        url: '/forecast',
+        templateUrl: './templates/forecast.html'
+    })
+});
