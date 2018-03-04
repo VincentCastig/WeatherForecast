37be6d0a1a817d69f9a3915b429ec83e10418d11
VincentCastig <vincent.castig@gmail.com>
2018-03-04 13:07:39 -0600

    Count Days Parameter

--- app.js
+++ app.js
@@ -15,16 +15,21 @@ var myApp = angular.module('myApp', ['ui.router', 'ngResource'])
         templateUrl: './templates/forecast.html',
         controller: 'forecastController'
     })
+    .state('forecast/:days', {
+        url: '/forecast/:days',
+        templateUrl: './templates/forecast.html',
+        controller: 'forecastController'
+    })
 });
 
 //services
 myApp.service('cityService', function($http) {
     this.city = 'Berlin';
 
-    this.getWeather = (city) => {
+    this.getWeather = (city, days) => {
         return $http({
         method: "GET",
-        url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=3&APPID=64c25a5bbade86005c613ed5edc028a5`
+        url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=${days}&APPID=64c25a5bbade86005c613ed5edc028a5`
         }).then((response) => {
             return response.data;
         })
@@ -39,15 +44,16 @@ myApp.controller('homeController', function($scope, cityService){
     })
 });
 
-myApp.controller('forecastController', function($scope, $resource, cityService){
+myApp.controller('forecastController', function($scope, cityService, $stateParams){
     $scope.city = cityService.city;
 
     // $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/weather?APPID=64c25a5bbade86005c613ed5edc028a5", {
     //     callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }
     // });
+    $scope.days = $stateParams.days || 3;
+    console.log($stateParams.days)
 
-    
-    cityService.getWeather($scope.city).then(function(res) {
+    cityService.getWeather($scope.city, $scope.days).then(function(res) {
         console.log(res)
         $scope.weatherResult = res;
     });
